0cb4e2bbd0b4757f4b925859e52ebbfcf951cf32
Siarhei Shpak <shpaksv@gmail.com>
2022-03-23 16:59:08 -0400

    Update rand-fourier-pb.py

--- rand-fourier-pb.py
+++ rand-fourier-pb.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-t = np.arange(0.00, 10*np.pi, 0.05)
+t = np.arange(0.00, 2*np.pi, 0.05)
 data = pd.DataFrame({
   "sin+rand": np.random.uniform(-1,1, [1, len(t)])[0] + (4 / np.pi) * (np.sin(t) + np.sin(3*t)/3 + np.sin(5*t)/5),
   "sin": (4 / np.pi) * (np.sin(t) + np.sin(3*t)/3 + np.sin(5*t)/5),
