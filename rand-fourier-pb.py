a6427da7cdb89ccc8c523e6b28f08f7457e952bb
Siarhei Shpak <shpaksv@gmail.com>
2022-03-23 17:26:01 -0400

    Update rand-fourier-pb.py

--- rand-fourier-pb.py
+++ rand-fourier-pb.py
@@ -4,7 +4,7 @@ import numpy as np
 
 t = np.arange(0.00, 4*np.pi, 0.05)
 data = pd.DataFrame({
-  "sin+rand": 0.2*np.random.uniform(-1,1, [1, len(t)])[0] + (4 / np.pi) * (np.sin(t) + np.sin(3*t)/3 + np.sin(5*t)/5),
+  "sin+rand": 0.5*np.random.uniform(-1,1, [1, len(t)])[0] + (4 / np.pi) * (np.sin(t) + np.sin(3*t)/3 + np.sin(5*t)/5),
   "sin": (4 / np.pi) * (np.sin(t) + np.sin(3*t)/3 + np.sin(5*t)/5),
   # "rand": 0.2*np.random.uniform(-1,1, [1, len(t)])[0] - 5,
   "t": t
